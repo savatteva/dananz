b2b2420ae5cfa217ec0769ecb428f2d5bad4c230
Polina Savateeva <polina.savateeva@geogracom.com>
2025-03-27 15:48:17 +0300

    added typography

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,14 @@
 import { useTranslation } from "react-i18next"
 import styles from "./styles.module.css"
+import { Typography } from "../typography";
 
 function App() {
   const { t } = useTranslation("mainPage");
   return (
     <>
       <p className={styles.test}>{t("test")}</p>
+
+      <Typography text="jib"/>
     </>
   )
 }
---
+++ src/components/typography/type.ts
@@ -0,0 +1,12 @@
+type TTypographyTag = "h1" | "h2" | "h3" | "p";
+type TTypographyColor = "mainColor" | "lightColor";
+type TTypographyWeight = "weight700" | "weight600" | "regular";
+type TTypographySize = "littleSize" |"regularSize" | "size24" | "size36" | "size72";
+
+export type TTypographyProps = {
+  Tag?: TTypographyTag;
+  text: string;
+  color?: TTypographyColor;
+  weight?: TTypographyWeight;
+  size?: TTypographySize;
+}
