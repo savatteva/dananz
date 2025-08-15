4d07dda7f33648cacd18c9b25c59e4b4077a5178
Polina Savateeva <polina.savateeva@geogracom.com>
2025-08-16 00:17:59 +0300

    test

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router';
+import styles from './styles.module.css';
+import { Header } from '@widgets';
+import { MainPage } from '@pages';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { App } from './app';
+import { Button } from './button';
+import { Typography } from './typography';
+
+export { Typography, Button, App };
